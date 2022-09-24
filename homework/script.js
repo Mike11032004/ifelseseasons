e2c505ca7574f1f3e8b07a7f83d8cfd93d9fd3f8
Munis <xamrakulovmunis@gmail.com>
2022-09-24 15:10:15 +0500

    new project

---
+++ homework/script.js
@@ -0,0 +1,27 @@
+let decade = prompt(' Any number from 1 to 29')
+
+if (decade <= 9) {
+    alert('1 decade')
+} else if (decade <= 19 && '> 9') {
+    alert('2 decade')
+} else if (decade <= 29 && '> 19') {
+    alert('3 decade')
+} else {
+    alert('mistake')
+}
+
+
+let season = prompt('Any number from 1 to 12') 
+
+if (season >= 2 && '=== 12') {
+    alert('Winter')
+} else if (season <= 3 || '>= 5') {
+    alert('Spring')
+} else if (season <= 6 || '>= 8') {
+    alert('Suumer')
+} else if (season <= 9 || '>= 11') {
+    alert('Fall') 
+} else {
+    alert('IDK')
+}
+
